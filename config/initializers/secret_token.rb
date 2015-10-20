bfe42bdf85736094da425e73c5adabaf32a9bfc0
Rebecca Taylor <inflamedmuse@gmail.com>
2015-10-20 21:52:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quoty::Application.config.secret_key_base = 'ed163d793dae8b9dfafa446df40e6307e62b6e46c02e265927cb9bdd31339072e5915e2e2d81ac4fd36d903490e1191e09b1cb5d00dd2d9a78615ab8a3a5f00b'
