070a751114b58aa4032580985552c8881ff3990c
Rebecca Taylor <inflamedmuse@gmail.com>
2015-11-05 08:09:22 +0000

    Added unique initials with corresponding ID to index quote page

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+  	quote = Quote.create(:author => 'Rebecca Taylor', :saying => 'Testing testing, yo.')
+  	expected = 'RT#' + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
   end
 end
