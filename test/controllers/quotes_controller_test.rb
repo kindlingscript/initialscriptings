2166d7c25cc34fc5acd60677ffe20f60161d676c
Rebecca Taylor <inflamedmuse@gmail.com>
2015-11-06 09:24:33 +0000

    Built up show page for individual quotes

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote = Quote.create(:author => 'Rebecca Taylor', :saying => 'Testing testing, yo.')
+    get :show, :id => quote.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'nah'
+  	assert_response :not_found
+  end
 end
